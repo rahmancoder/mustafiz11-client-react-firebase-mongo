bb5aa8e78bbbde5a3b648db7ca32e4c311ac55cf
Mustafizur <sayhemrahman@gmail.com>
2021-10-29 02:14:25 +0600

    client side setup with react dom react bootstrap firebase setup with initial config react hook form structure file setup done

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCjUr5Pn7yDBiQAoP6Hr9iCTlscMrWPQw",
+    authDomain: "mustafiz-tourism-website.firebaseapp.com",
+    projectId: "mustafiz-tourism-website",
+    storageBucket: "mustafiz-tourism-website.appspot.com",
+    messagingSenderId: "177924358770",
+    appId: "1:177924358770:web:a4c0ddecb4d971b724b8eb"
+};
+
+export default firebaseConfig;
