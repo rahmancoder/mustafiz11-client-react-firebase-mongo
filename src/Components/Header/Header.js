1fc52dd93c8d1c4d87bb421cbc563df6b166f0e2
Mustafizur <sayhemrahman@gmail.com>
2021-10-29 17:24:08 +0600

    usefirebase useauth problems

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
+
+import { Link } from 'react-router-dom';
+import { Container, Navbar, Button, Nav } from 'react-bootstrap';
 
 const Header = () => {
+    const { logOut, user } = useFirebase();
     return (
-        <div>
-            <h1>This is header</h1>
+        <>
+            <Navbar sticky="top" bg="warning" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/home"><i class="fas fa-briefcase-medical"></i> HealthCare Online & Support</Navbar.Brand>
+                    <Nav className="me-5 p-2 text-center">
+                        <Button className="bg-warning"><Link className="m-2 text-decoration-none" to="/home">Home</Link> </Button>
+                        <Link className="m-2 text-decoration-none text-light" to="/services">Services</Link>
+                        <Link className="m-2 text-decoration-none text-light" to="/doctors">Doctor</Link>
+                        <Link className="m-2 text-decoration-none text-light" to="/private">PrivateDoctor</Link>
+                        {/* <Link className="m-2 text-decoration-none text-light" to="/login">LogIn</Link> */}
+                        {/* <Link className="m-2 text-decoration-none text-light" to="/register">Register</Link> */}
+                        {user?.email ? (
 
-        </div>
+                            <Link className="m-2 text-decoration-none text-light" to="/services">
+                                <li onClick={logOut} className="header-items">
+                                    Logout
+                                </li>
+                            </Link>
+
+                        ) : (
+                            <Link to="/login" className="header-items m-2 text-decoration-none text-light">
+                                Login
+                            </Link>
+                        )}
+                        {
+                            user?.email && <Link className="m-2 text-decoration-none text-light" to="/">
+                                <li className="header-items">
+                                    {user.displayName}
+                                </li>
+                            </Link>
+                        }
+                        <Link className="m-2 text-decoration-none text-light" to="/questions">FAQ</Link>
+                        {/* <Link className="m-2" to="/notfound">Not Found</Link> */}
+                    </Nav>
+                </Container>
+            </Navbar>
+
+
+        </>
     );
 };
 
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
+
+const Login = () => {
+    const { signInUsingGoogle,
+        user,
+        //     handleResetPassword,
+        //     verifyEmail,
+        //     setUserName,
+        //     registerNewUser,
+        //     processLogin,
+        // handleGoogleSignIn,
+        logOut
+    } = useFirebase();
+
+
+    // const handleGoogleSignIn = () => {
+    //     signInWithPopup(auth, googleProvider)
+    //         .then(result => {
+    //             const user = result.user;
+    //             console.log(user);
+
+
+    //             const { displayName, email, photoURL } = result.user;
+    //             const loggedInUser = {
+    //                 name: displayName,
+    //                 email: email,
+    //                 photo: photoURL
+    //             };
+    //             setUser(loggedInUser);
+    //         })
+    //         .catch(error => {
+    //             console.log(error.message);
+    //         })
+    // }
+
+
+
+    return (
+        <div>
+            {!user.email ?
+                <div>
+                    {/* <button onClick={handleGoogleSignIn}>Google Sign In</button> */}
+                    {/* <button onClick={handleGithubSignIn}>Github Sign In</button> */}
+                    {/* <button onClick={handleFacebookSignIn}>Facebook Sign In</button> */}
+                </div> :
+                // <button onClick={handleSignOut}>Sign Out</button>
+            }
+            <br />
+
+
+            {
+                user.email && <div>
+                    <h2>Welcome {user.name}</h2>
+                    <p>Your Login Email is: {user.email}</p>
+                    <img src={user.photo} alt="" />
+                </div>
+            }
+
+        </div>
+    );
+};
+
+export default Login;
