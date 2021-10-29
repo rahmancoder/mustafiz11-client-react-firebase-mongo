e9f8cbc2b43bbd58bd36b014c953a388326ccb5c
Mustafizur <sayhemrahman@gmail.com>
2021-10-29 18:34:11 +0600

    mapbox gl added but got some error in library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotFound from './Components/NotFound/NotFound';
 import Home from './Components/Home/Home';
 import Vacation from './Components/Vacation/Vacation';
+// import MyMap from './Components/MyMap/MyMap';
 
 function App() {
   return (
@@ -20,9 +22,13 @@ function App() {
           {/* <Route path="/vacation">
             <Vacation></Vacation>
           </Route> */}
-          <Route path="/tourism">
+          {/* <Route path="/tourism">
 
-          </Route>
+          </Route> */}
+
+          {/* <Route path="/map">
+            <MyMap></MyMap>
+          </Route> */}
           <Route path="*">
             <NotFound></NotFound>
           </Route>
