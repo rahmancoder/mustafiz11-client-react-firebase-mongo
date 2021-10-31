3afb43e1545c02724ce45c778ec80c5c6e4373db
Mustafizur <sayhemrahman@gmail.com>
2021-10-31 14:03:46 +0600

    banner section added with images

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import banner1 from '../../images/Banner/banner1.jpg';
+import banner2 from '../../images/Banner/banner2.jpg';
+import banner3 from '../../images/Banner/banner3.jpg';
 
 const Banner = () => {
     return (
         <div>
-            <h1>This is banner</h1>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Travel And Tourism</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>World Travel/Tour</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Adventure Travel</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
